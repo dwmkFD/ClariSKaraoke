6d9eef6e822531c1e3cfbe1d990bea461d22f3e0
FD（えふでぃ） <fd.dwmk@gmail.com>
2024-10-29 13:47:22 +0900

    2nd commit

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,6 +8,8 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
+using MudBlazor.Services;
+
 namespace ClariSKaraoke.Client
 {
     public class Program
@@ -18,6 +20,7 @@ namespace ClariSKaraoke.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
